b9dd9175e843df255daa42c07646b19b4f2b8540
Takuto Ikuta <atetubou@gmail.com>
2017-11-25 01:24:47 +0000

    distribute fix

--- ssh/distribute.sh
+++ ssh/distribute.sh
@@ -21,7 +21,7 @@ do
 	# echo $pub
 	sshpass -p $1 ssh-copy-id -f -i $pub -o "StrictHostKeyChecking no" $hostname
     done
-    ssh ${hosntame} mkdir ~/.ssh
-    scp isucon_rsa ${hosname}:~/.ssh/id_rsa
-    scp isucon_rsa.pub ${hosname}:~/.ssh/id_rsa.pub
+    ssh ${hostname} mkdir -p ~/.ssh 
+    scp isucon_rsa ${hostname}:~/.ssh/id_rsa
+    scp isucon_rsa.pub ${hostname}:~/.ssh/id_rsa.pub
 done
